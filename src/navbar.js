f38deda35f405076c7addffe3d0831d248cb2a12
duainem7 <duainem7@gmail.com>
2024-04-14 14:20:44 -0400

    latest fixes

--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,3 @@
-import React, {useState} from "react"
 import './App.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
--- src/selfie.js
+++ src/selfie.js
@@ -1,10 +1,7 @@
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container';
-import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
 import './App.css'
-import palm from './palm.jpg'
 
 function SelfieImage(){
     return(
@@ -12,7 +9,7 @@ function SelfieImage(){
             <Row>
                 <Col>
                 
-                <iframe src="https://giphy.com/embed/7ZCmbMf3z6iP898BfD" title="" width="480" height="480" frameBorder="0" class="giphy-embed" style={{pointerEvents: "none"}}></iframe>
+                <iframe src="https://giphy.com/embed/7ZCmbMf3z6iP898BfD" title=" " width="480" height="480" frameBorder="0" class="giphy-embed" style={{pointerEvents: "none"}}></iframe>
                 
                 </Col>
             </Row>
