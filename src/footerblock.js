b64f819e1c469c821bc77a8392043b2adf1c419a
duainem7 <duainem7@gmail.com>
2024-04-16 19:12:22 -0400

    latest commit

--- src/footerblock.js
+++ src/footerblock.js
@@ -1,19 +1,17 @@
 import './App.css'
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import remote from './remote.png'
-import philly from './philly.png'
+
 
 
 function FooterBlock() {
     return(
         <Container className='footerblock' style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
 
-            <h2 className="footertext" style={{color: "whitesmoke", paddingTop: "70px", paddingLeft: "23px", fontFamily: "Pacifico"}}>Servicing:</h2>
-            <Container className="d-flex p-3">
-                <Image className="footerpics" src={remote} />
-                <Image className="footerpics" src={philly} />
-            </Container>
+            <h2 className="footertext" style={{color: "whitesmoke", paddingTop: "70px", paddingLeft: "23px", fontFamily: "Pacifico"}}>Servicing: Remotely + in Philadelphia ❤️</h2>
+            
+                
+            
         </Container>
     )
 }
